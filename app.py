3035ddabe482b108b98b58b6c077294fe214f252
Cezary Sieczkowski <s22595@pjwstk.edu.pl>
2024-06-01 13:31:09 +0200

    main app done

--- app.py
+++ app.py
@@ -18,6 +18,39 @@ app = FastAPI()
 def index():
     return {"message": "Linear Regression ML"}
 
-@app.post("/model/point", tags=["data"],response_model=Point,status_code=200)
+@app.post("/model/point", tags=["data"], response_model=Point, status_code=200)
 async def point(x: Annotated[int,Form()],y: Annotated[int,Form()]):
     return Point(x=x,y=y)
+
+@app.post("/model/train",tags=["model"],status_code=200)
+async def train_model(data:Point, data_name="10_points", model_name="our_model"):
+    data_file=Path(DATA_DIR).joinpath(f"{data_name}.csv")
+    model_file=Path(MODEL_DIR).joinpath(f"{model_name}.pkl")
+
+    data = data.dict()
+    x = data[x]
+    y = data[y]
+
+    train(x, y, data_file, model_file)
+
+    response_object = {"model_fit": "Ok","model_save": "Ok"}
+    return response_object
+
+@app.post("/model/predict", tags=["model"], response_model=Point, status_code=200)
+async def get_prediction(data:Point, model_name="our_model"):
+    model_file = Path(MODEL_DIR).joinpath(f"{model_file}.pkl")
+
+    if not model_file.exists():
+        raise HTTPException(status_code=400, detail="Model not found.")
+
+    data = data.dict()
+    x = data["x"]
+
+    y_pred = predict(x=x,ml_model=model_file)
+
+    data["y"] = y_pred
+
+    response_object = {"x": x, "y": y_pred[0][0]}
+    
+if __name__ == "__name__":
+    uvicorn.run(app, host='127.0.0.1',port=8008)
